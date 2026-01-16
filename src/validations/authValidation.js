2b5b6552f4a89fedb8f3f200b5e4b13bb7e8c807
Anton Shuvalov <anshuvalov83@gmail.com>
2026-01-16 13:17:41 +0200

    add: auth(registration, login, authorization, logout), authController, time, authenticate, model-session, auth-route, auth-services, authValidation

---
+++ src/validations/authValidation.js
@@ -0,0 +1,25 @@
+import { Joi, Segments } from 'celebrate';
+
+export const registerUserSchema = {
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().min(2).max(32).required().message({
+      'string.base': 'Name must be a string',
+      'string.empty': 'Name is required',
+      'string.min': 'Name must be at least 2 characters long',
+      'string.max': 'Name must be at most 32 characters long',
+      'any.required': 'Name is required',
+    }),
+    email: Joi.string().email().max(64).required().message({
+      'string.email': 'Invalid email format',
+      'any.required': 'Email is required',
+    }),
+    password: Joi.string().min(8).max(64).required(),
+  }),
+};
+
+export const loginUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().max(64).required(),
+    password: Joi.string().min(8).max(64).required(),
+  }),
+};
